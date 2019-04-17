67cb465af879b7bbfb92c80812d9ff5e163c2120
Vincent Houillon <vhouillon@gmail.com>
2019-04-17 09:46:01 +0200

    create models

--- app/forms.py
+++ app/forms.py
@@ -11,4 +11,4 @@ class SignupForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2',)
+        fields = ('username', 'last_name', 'first_name', 'email', 'password1', 'password2',)
--- app/models.py
+++ app/models.py
@@ -1,33 +1,57 @@
+# https://docs.djangoproject.com/fr/2.2/topics/db/models/
 from django.db import models
 
-# https://docs.djangoproject.com/fr/2.2/topics/db/models/
-class Categories(models.Model):
-    """Manage OpenFoodFacts Food Categories
+
+class Categorie(models.Model):
+    """OpenFoodFacts Food Categories
     """
-    category_name = models.CharField(max_length=255, unique=True)
+    category_name = models.CharField(
+        max_length=255, unique=True, verbose_name='catégorie')
 
     def __str__(self):
         return self.category_name
 
+    class Meta:
+        verbose_name = 'catégorie'
 
-class Products(models.Model):
-    """Food details retrieved from OpenFoodFacts and saved by the user
+
+class Nutriscore(models.Model):
+    """OpenFoodFacts Food Nutriscores
     """
-    product_name = models.CharField(max_length=255)
-    url = models.URLField()
-    image_url = models.URLField()
-    fat = models.DecimalField(max_digits=5, decimal_places=2, null=True)
-    satured_fat = models.DecimalField(max_digits=5, decimal_places=2, null=True)
-    sugars = models.DecimalField(max_digits=5, decimal_places=2, null=True)
-    salt = models.DecimalField(max_digits=5, decimal_places=2, null=True)
-    nutrition_grades = models.CharField(max_length=1, null=True)
-    category = models.ForeignKey("Categories", on_delete=models.CASCADE)
+    nutrition_grades = models.CharField(
+        max_length=10, unique=True, verbose_name='nutriscore')
 
     def __str__(self):
-        return str({
-            "name": self.product_name,
-            "url": self.url,
-            "image": self.image_url,
-            "nutriscore": self.nutrition_grades,
-            "category": self.category
-        })
+        return self.nutrition_grades
+
+    class Meta:
+        verbose_name = 'nutriscore'
+
+
+class Product(models.Model):
+    """Food details retrieved from OpenFoodFacts and saved by the user
+    """
+    product_name = models.CharField(
+        max_length=255, verbose_name='nom du produit')
+    brands = models.CharField(max_length=255, null=True, verbose_name='marque')
+    quantity = models.CharField(
+        max_length=40, null=True, verbose_name='quantité')
+    code = models.CharField(max_length=40, unique=True, verbose_name='code barre')
+    url = models.URLField()
+    image_url = models.URLField(
+        null=True, verbose_name='lien vers la page OFF', help_text='OFF : Open Food Facts')
+    fat = models.DecimalField(max_digits=5, decimal_places=2,
+                              null=True, verbose_name='matières grasses pour 100g')
+    satured_fat = models.DecimalField(
+        max_digits=5, decimal_places=2, null=True, verbose_name='acides gras saturés pour 100g')
+    sugars = models.DecimalField(
+        max_digits=5, decimal_places=2, null=True, verbose_name='sucres pour 100g')
+    salt = models.DecimalField(
+        max_digits=5, decimal_places=2, null=True, verbose_name='sel pour 100g')
+    nutriscore = models.ForeignKey(
+        "Nutriscore", on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        "Categorie", on_delete=models.CASCADE, verbose_name='catégorie')
+
+    class Meta:
+        verbose_name = 'produit'
