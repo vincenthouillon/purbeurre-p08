87f8dafd374f447b02b1e3a2ae65ffa875589b26
Vincent Houillon <vhouillon@gmail.com>
2019-04-29 17:53:33 +0200

    add django_heroku.settings(locals())

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -129,3 +131,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+django_heroku.settings(locals())
