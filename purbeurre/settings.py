0097359709658352d471f45abd7bec0dfda1fe61
Vincent Houillon <vhouillon@gmail.com>
2019-05-06 11:27:08 +0200

    a

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -128,7 +128,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
