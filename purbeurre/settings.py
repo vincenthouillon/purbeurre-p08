552433f00f5400c4dc71e6163e4fda3cf531b7ba
Vincent Houillon <vhouillon@gmail.com>
2019-05-06 09:31:41 +0200

    edit settings

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 print(f'DEBUG: {DEBUG}')
 
+
 ALLOWED_HOSTS = ['hv-purbeurre-app.herokuapp.com', '*']
 
 
@@ -127,7 +128,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
