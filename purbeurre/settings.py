4325fcd2a7db18e343d24859503cc3d930504f8d
Vincent Houillon <vhouillon@gmail.com>
2019-05-06 11:25:22 +0200

    update settings.py

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -127,6 +127,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
